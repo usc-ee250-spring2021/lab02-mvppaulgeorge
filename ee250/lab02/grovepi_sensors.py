e48953bed8b0f75081663a53879afa3ca0857c91
mvppaulgeorge <Your @usc.edu email>
2021-02-07 15:14:47 +0000

    modified lab code

--- ee250/lab02/grovepi_sensors.py
+++ ee250/lab02/grovepi_sensors.py
@@ -23,6 +23,8 @@ sys.path.append('../../Software/Python/')
 sys.path.append('../../Software/Python/grove_rgb_lcd')
 
 import grovepi
+from grovepi import *
+from grove_rgb_lcd import *
 
 """This if-statement checks if you are running this python file directly. That 
 is, if you run `python3 grovepi_sensors.py` in terminal, this if-statement will 
@@ -34,18 +36,25 @@ if __name__ == '__main__':
     i = 0
     ultrasonic_ranger = 4
     while True:
+	try:
         #So we do not poll the sensors too quickly which may introduce noise,
         #sleep for a reasonable time of 200ms between each iteration.
-        time.sleep(0.2)
-	distant=ultrasonicRead(ultrasonic_ranger)
-	print distant, "cm"
+            time.sleep(0.2)
+	    distant = ultrasonicRead(ultrasonic_ranger)
+	    print distant, "cm"
+	    ultra = str(distant)
 
-	i=grovepi.analogRead(potentiometer)
-	print i, "cm"
+	    i = grovepi.analogRead(potentiometer)
+	    print i, "cm"
+	    
+	    rotary = str(abs(i))
+	    setRGB(0,128,64)
+	    setRGB(0,255,0)
+	    if distant<i:
+	        setText_norefresh(rotary+"cm"+"OBJ PRES\n"+ultra+"cm")
+	    elif i<distant:
+		setText_norefresh(rotary+"cm"+"           \n"+ultra+"cm")
 
-	setRGB(0,128,64)
-	setRGB(0,255,0)
-
-	setText(i+"cm"+distant+"cm")
-
-        print(grovepi.ultrasonicRead(PORT))
+            print(grovepi.ultrasonicRead(PORT))
+	except (IOError,TypeError) as e:
+	    print "Error"
